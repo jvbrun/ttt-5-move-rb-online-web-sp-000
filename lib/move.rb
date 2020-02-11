1939f380353f995ebc2b9047d58f44bdba200b17
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-11 04:40:38 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,6 +14,6 @@ def input_to_index(input)
   converted_input = input - 1
 end
 
-def move(board, converted_input)
-  board[conveted_input] = "X"
+def move(board, input_to_index)
+  board[input_to_index] = "X"
 end
