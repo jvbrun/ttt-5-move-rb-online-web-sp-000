4c01122c8c62261277884bd86f470aa413862c06
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-11 04:19:36 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def board 
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+end
 
 # code your input_to_index and move method here!
 def input_to_index(input)
