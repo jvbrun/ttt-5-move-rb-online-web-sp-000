346968d083eda0cc6876dc6a09580f19804fdb70
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-11 03:27:51 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,7 +7,7 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(move)
-  move = move.to_i 
-  converted_input = move - 1
+def input_to_index(input)
+  input = input.to_i 
+  converted_input = input - 1
 end
