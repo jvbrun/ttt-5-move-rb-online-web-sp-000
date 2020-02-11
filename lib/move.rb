c0a0b486260c42b1154681bcb5eeff012c15e347
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-11 03:25:02 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,4 @@
-def display_board(board)
+def display_board(board[input_to_index])
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -10,5 +10,4 @@ end
 def input_to_index(move)
   move = move.to_i 
   converted_input = move - 1
-  board = converted_input
 end
